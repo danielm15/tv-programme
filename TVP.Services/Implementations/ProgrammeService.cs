9e09218f469548d1f7ba4a1a56f7290ebc4937af
danielm15 <danielm15@ru.is>
2020-12-17 00:53:29 +0000

    WebApp basic functionality completed

--- TVP.Services/Implementations/ProgrammeService.cs
+++ TVP.Services/Implementations/ProgrammeService.cs
@@ -2,6 +2,8 @@ using TVP.Services.Interfaces;
 using TVP.Repositories.Interfaces;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using TVP.Models.Dtos;
 
 namespace TVP.Services.Implementations
@@ -15,6 +17,11 @@ namespace TVP.Services.Implementations
             _apiDataCollector = apiDataCollector;
         }
 
+        public async Task<IEnumerable<String>> GetStod2Channels()
+        {
+            return await _apiDataCollector.GetStod2Channels();
+        }
+
         public async Task<IEnumerable<ProgrammeItemDto>> GetStod2ProgrammeForChannel(string channel)
         {
             return await _apiDataCollector.GetStod2ProgrammeForChannel(channel);
@@ -23,6 +30,13 @@ namespace TVP.Services.Implementations
         public async Task<IEnumerable<RuvProgrammeItemDto>> GetRUVProgramme()
         {
             return await _apiDataCollector.GetRUVProgramme();
+        }
+
+        public IEnumerable<ProgrammeItemDto> FilterProgrammeByDate(IEnumerable<ProgrammeItemDto> programmeList, DateTime inputDate)
+        {
+            return from item in programmeList
+                where item.Date == inputDate
+                select item;
         }
     }
 }
--- TVP.Services/Interfaces/IProgrammeService.cs
+++ TVP.Services/Interfaces/IProgrammeService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 using TVP.Models.Dtos;
 using System.Threading.Tasks;
 
@@ -6,7 +7,9 @@ namespace TVP.Services.Interfaces
 {
     public interface IProgrammeService
     {
+        Task<IEnumerable<String>> GetStod2Channels();
         Task<IEnumerable<ProgrammeItemDto>> GetStod2ProgrammeForChannel(string channel);
         Task<IEnumerable<RuvProgrammeItemDto>> GetRUVProgramme();
+        IEnumerable<ProgrammeItemDto> FilterProgrammeByDate(IEnumerable<ProgrammeItemDto> programmeList, DateTime inputDate);
     }
 }
